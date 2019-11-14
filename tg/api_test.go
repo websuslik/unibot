ef19c3f3b88af81e59032b41281b3d9aa1c22694
Yuri Voronkov <y.voronkov@lucky-labs.com>
2019-11-14 20:15:07 +0200

    Add test for the GetUpdates and TestSetWebhook methods

--- tg/api_test.go
+++ tg/api_test.go
@@ -1,23 +1,55 @@
 package tg_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/websuslik/unibot/tg"
-	"io/ioutil"
-	"net/http"
-	"strings"
 	"testing"
 	"time"
 )
 
-type HTTPClientMock struct {
+var commonUser = map[string]interface{}{
+	"id":         123,
+	"is_bot":     false,
+	"first_name": "Yuri",
+}
+
+var commonChat = map[string]interface{}{
+	"id":   123,
+	"type": "private",
+}
+
+var commonMessage = map[string]interface{}{
+	"message_id": 123,
+	"from":       commonUser,
+	"date":       123,
+	"chat":       commonChat,
+	"text":       "Hello, World!",
+}
+
+var commonTrueResponse = map[string]interface{}{
+	"ok":     true,
+	"result": true,
+}
+
+type HttpClientMock struct {
 	mock.Mock
 }
 
-func (m *HTTPClientMock) Do(req *http.Request, timeout time.Duration) (*http.Response, error) {
-	args := m.Called(req)
-	return args.Get(0).(*http.Response), args.Error(1)
+func (m *HttpClientMock) Do(url string, args *tg.RequestArgs, timeout time.Duration) ([]byte, error) {
+	callArgs := m.Called(url, args, timeout)
+	return callArgs.Get(0).([]byte), callArgs.Error(1)
+}
+
+func setUpMock(method string, bodyData map[string]interface{}) (*HttpClientMock, *tg.API) {
+	m := new(HttpClientMock)
+	body, _ := json.Marshal(bodyData)
+	url := fmt.Sprintf("https://api.telegram.org/botTOKEN/%s", method)
+	m.On("Do", url, mock.Anything, tg.Timeout*time.Second).Return(body, nil)
+	api := &tg.API{Token: "TOKEN", Client: m}
+	return m, api
 }
 
 func TestChatID(t *testing.T) {
@@ -35,23 +67,33 @@ func TestChatID(t *testing.T) {
 		assert.Nil(t, e)
 	}
 }
-func setMockResponseBody(m *HTTPClientMock, body string) {
-	m.On("Do", mock.Anything).Return(&http.Response{
-		Body: ioutil.NopCloser(strings.NewReader(body))}, nil)
-}
+
 func TestGetUpdates(t *testing.T) {
+	m, api := setUpMock("getUpdates", map[string]interface{}{
+		"ok": true,
+		"result": []interface{}{
+			map[string]interface{}{
+				"update_id": 123,
+				"message":   commonMessage,
+			},
+		},
+	})
 	args := &tg.GetUpdatesArgs{}
-	req, _ := args.GetRequestArgs()
-	assert.Equal(t, req.Headers, map[string]string{"Content-Type": "application/json"})
-
-	client := new(HTTPClientMock)
-	setMockResponseBody(client, "{\"ok\": true, \"result\": [{}, {}]}")
-
-	api := &tg.API{
-		Token:  "TOKEN",
-		Client: client,
-	}
 	res, err := api.GetUpdates(args)
+	m.AssertExpectations(t)
 	assert.Nil(t, err)
-	assert.Equal(t, len(*res), 2)
+	assert.Equal(t, len(*res), 1)
+	for _, value := range *res {
+		assert.Equal(t, value.UpdateID, 123)
+		assert.Equal(t, value.Message.Text, "Hello, World!")
+	}
+}
+
+func TestSetWebhook(t *testing.T) {
+	m, api := setUpMock("setWebhook", commonTrueResponse)
+	args := &tg.SetWebhookArgs{}
+	res, err := api.SetWebhook(args)
+	m.AssertExpectations(t)
+	assert.Nil(t, err)
+	assert.Equal(t, *res, true)
 }
