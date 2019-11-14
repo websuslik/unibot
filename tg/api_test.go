dba739967d4370623675b2f52257555b320119c8
Yuri Voronkov <y.voronkov@lucky-labs.com>
2019-11-14 17:40:04 +0200

    Add test for the GetUpdates method

--- tg/api_test.go
+++ tg/api_test.go
@@ -2,9 +2,23 @@ package tg_test
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/websuslik/unibot/tg"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+	"time"
 )
+
+type HTTPClientMock struct {
+	mock.Mock
+}
+
+func (m *HTTPClientMock) Do(req *http.Request, timeout time.Duration) (*http.Response, error) {
+	args := m.Called(req)
+	return args.Get(0).(*http.Response), args.Error(1)
+}
 
 func TestChatID(t *testing.T) {
 	tests := []struct {
@@ -20,4 +34,24 @@ func TestChatID(t *testing.T) {
 		assert.Equal(t, v, test.result)
 		assert.Nil(t, e)
 	}
+}
+func setMockResponseBody(m *HTTPClientMock, body string) {
+	m.On("Do", mock.Anything).Return(&http.Response{
+		Body: ioutil.NopCloser(strings.NewReader(body))}, nil)
+}
+func TestGetUpdates(t *testing.T) {
+	args := &tg.GetUpdatesArgs{}
+	req, _ := args.GetRequestArgs()
+	assert.Equal(t, req.Headers, map[string]string{"Content-Type": "application/json"})
+
+	client := new(HTTPClientMock)
+	setMockResponseBody(client, "{\"ok\": true, \"result\": [{}, {}]}")
+
+	api := &tg.API{
+		Token:  "TOKEN",
+		Client: client,
+	}
+	res, err := api.GetUpdates(args)
+	assert.Nil(t, err)
+	assert.Equal(t, len(*res), 2)
 }
