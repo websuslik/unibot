bee0dee548f631abe8a780f27a71517781cd777a
Yuri Voronkov <y.voronkov@lucky-labs.com>
2019-11-14 14:43:59 +0200

    Move tests to a different package

--- tg/api_test.go
+++ tg/api_test.go
@@ -1,18 +1,19 @@
-package tg
+package tg_test
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/websuslik/unibot/tg"
 	"testing"
 )
 
 func TestChatID(t *testing.T) {
 	tests := []struct {
-		chatID *ChatID
+		chatID *tg.ChatID
 		result []byte
 	}{
-		{chatID: &ChatID{ID: 123}, result: []byte("123")},
-		{chatID: &ChatID{ID: 123, Username: "@hello"}, result: []byte("123")},
-		{chatID: &ChatID{Username: "@hello"}, result: []byte("@hello")},
+		{chatID: &tg.ChatID{ID: 123}, result: []byte("123")},
+		{chatID: &tg.ChatID{ID: 123, Username: "@hello"}, result: []byte("123")},
+		{chatID: &tg.ChatID{Username: "@hello"}, result: []byte("@hello")},
 	}
 	for _, test := range tests {
 		v, e := test.chatID.MarshalJSON()
